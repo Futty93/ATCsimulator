752cfe588dd64d0e9f5e9b4acab1190aa71678af
Futty93 <kazuki.futawatari.s5@dc.tohoku.ac.jp>
2023-05-28 21:46:49 +0900

    Add files via upload

---
+++ AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  ATCSimulator
+//
+//  Created by 二渡和輝 on 2023/05/28.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
