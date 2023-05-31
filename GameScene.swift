53bce20711998854838caefd8add703ae12d2d66
Futty93 <kazuki.futawatari.s5@dc.tohoku.ac.jp>
2023-05-31 22:28:10 +0900

    Add files via upload

--- GameScene.swift
+++ GameScene.swift
@@ -8,81 +8,123 @@
 import SpriteKit
 import GameplayKit
 
+class ControlPanelScene: SKScene {
+    override func didMove(to view: SKView) {
+        backgroundColor = .gray
+        
+        // Add your control panel UI elements here
+    }
+}
+
 class GameScene: SKScene {
-    var aircraftNodes: [SKShapeNode] = []
-    var numberOfAircraft: Int = 8
-    var movementSpeed: CGFloat = 1.0 // Pixels per second
+    let flightNumbers = ["ANA", "JAL", "APJ", "SKY", "ADO", "FDA", "SFJ", "SNJ", "JTA"]
+    var aircraftNodes: [Aircraft] = []
+    var numberOfAircraft: Int = 8 //表示する航空機の数
+    
     
     override func didMove(to view: SKView) {
-        let aircraftSize = CGSize(width: 10, height: 10)
-        //        let spacing: CGFloat = 100
         
-        for i in 0..<numberOfAircraft {
-            let aircraftNode = SKShapeNode(rectOf: aircraftSize)
-            //            aircraftNode.fillColor = .white
-            // Set random initial position
-            let randomX = CGFloat.random(in: 0...(frame.width - aircraftSize.width))
-            let randomY = CGFloat.random(in: 0...(frame.height - aircraftSize.height))
-            aircraftNode.position = CGPoint(x: randomX, y: randomY)
-            
-            // Add number label
-            let labelNode = SKLabelNode(text: "\(i+1)")
-            labelNode.position = CGPoint(x: aircraftNode.position.x, y: aircraftNode.position.y - 30)
-            addChild(labelNode)
-            
-            addChild(aircraftNode)
-            aircraftNodes.append(aircraftNode)
+        
+        for _ in 0..<numberOfAircraft {
+            createRandomAircraft()
         }
     }
     
-    //    override func update(_ currentTime: TimeInterval) {
-    //        //        let deltaTime = CGFloat(self.deltaTime)
-    //
-    //        // Move each aircraft node horizontally
-    //        for aircraftNode in aircraftNodes {
-    //            aircraftNode.position.x += movementSpeed
-    //        }
-    //    }
-    
     override func update(_ currentTime: TimeInterval) {
-        //            let deltaTime = CGFloat(self.deltaTime)
         
         // Move each aircraft node towards the center
-        //        for aircraftNode in aircraftNodes {
-        //            let dx = frame.midX - aircraftNode.position.x
-        //            let dy = frame.midY - aircraftNode.position.y
-        //            let distanceToCenter = hypot(dx, dy)
-        //
-        //            // Move towards the center with a speed relative to the distance
-        //            let movementAmount = min(movementSpeed, distanceToCenter)
-        //            let angle = atan2(dy, dx)
-        //            let dxMove = movementAmount * cos(angle)
-        //            let dyMove = movementAmount * sin(angle)
-        //
-        //            aircraftNode.position.x += dxMove
-        //            aircraftNode.position.y += dyMove
-        //
-        //
-        //        }
-        for (index, aircraftNode) in aircraftNodes.enumerated() {
+        for aircraftNode in aircraftNodes {
             let dx = frame.midX - aircraftNode.position.x
             let dy = frame.midY - aircraftNode.position.y
             let distanceToCenter = hypot(dx, dy)
             
+            if distanceToCenter < 50 {
+                aircraftNode.removeFromParent()
+                aircraftNodes.removeAll(where: { $0 == aircraftNode })
+                aircraftNode.labelNode?.removeFromParent() // Remove label node from parent
+                createRandomAircraft()
+            }
+            
             // Move towards the center with a speed relative to the distance
-            let movementAmount = min(movementSpeed, distanceToCenter)
+            let movementAmount = min(aircraftNode.movementSpeed, distanceToCenter)
             let angle = atan2(dy, dx)
             let dxMove = movementAmount * cos(angle)
             let dyMove = movementAmount * sin(angle)
             
             aircraftNode.position.x += dxMove
             aircraftNode.position.y += dyMove
+            aircraftNode.labelNode?.position.x = 25
+            aircraftNode.labelNode?.position.y = 25
             
-            // Update label position
-            let labelNode = children[index * 2 + 1] as? SKLabelNode
-            labelNode?.position.x = aircraftNode.position.x
-            labelNode?.position.y = aircraftNode.position.y - 30
+            aircraftNode.labelNode?.text = "\(aircraftNode.flightCode)\nHeading: \(Int(aircraftNode.currentHeading * 180 / CGFloat.pi))"
             
+        }
+    }
+    
+    func createRandomAircraft() {
+        let center = CGPoint(x: size.width/2, y: size.height/2)
+        let aircraftSize = CGSize(width: 5, height: 5)
+        //        let spacing: CGFloat = 100
+        let aircraftNode = Aircraft(rectOf: aircraftSize)
+        aircraftNode.fillColor = .clear
+        aircraftNode.strokeColor = .white
+        
+        // Set random initial position
+        let randomX = CGFloat.random(in: 0...(frame.width - aircraftSize.width))
+        let randomY = CGFloat.random(in: 0...(frame.height - aircraftSize.height))
+        aircraftNode.position = CGPoint(x: randomX, y: randomY)
+//        aircraftNode.labelNode?.position = CGPoint(x: aircraftNode.position.x, y: aircraftNode.position.y - 30)
+        
+        //コールサインを生成
+        let randomFlightNumber = flightNumbers.randomElement() ?? ""
+        let randomFlightID = Int.random(in: 10...2000)
+        aircraftNode.flightCode = randomFlightNumber + String(randomFlightID)
+//        flightLabel.position = CGPoint(x: 25, y: 25)
+//        flightLabel.fontColor = .white
+//        flightLabel.fontSize = 12
+        let flightLabel = SKLabelNode(text: "\(aircraftNode.flightCode)\nHeading: \(Int(aircraftNode.currentHeading * 180 / CGFloat.pi))")
+//        flightLabel.position = CGPoint(x: 25, y: 25)
+        flightLabel.fontColor = .white
+        flightLabel.fontSize = 12
+        aircraftNode.addChild(flightLabel)
+        
+        aircraftNode.labelNode = flightLabel
+        
+        let directionToCenter = CGPoint(x: center.x - aircraftNode.position.x, y: center.y - aircraftNode.position.y)
+        aircraftNode.currentHeading = atan2(directionToCenter.y, directionToCenter.x)
+        
+        addChild(aircraftNode)
+//        addChild(aircraftNode.labelNode!)
+        aircraftNodes.append(aircraftNode)
+    }
+}
+
+
+class Aircraft: SKShapeNode {
+    var labelNode: SKLabelNode?
+    var movementSpeed: CGFloat = 0.5 // Pixels per second
+    var currentHeading: CGFloat = 0.0 // Initial heading towards the center of the screen
+    var flightCode: String = ""
+    
+    var isSelected: Bool = false {
+        didSet {
+            if isSelected {
+                fillColor = .green
+            } else {
+                fillColor = .clear
+            }
+        }
+    }
+    
+    override var isUserInteractionEnabled: Bool {
+        set {}
+        get { return true }
+    }
+    
+    override func mouseDown(with event: NSEvent) {
+        let touchLocation = event.location(in: self)
+        if contains(touchLocation) {
+            isSelected.toggle()
         }
     }
 }
--- ViewController.swift
+++ ViewController.swift
@@ -9,28 +9,70 @@ import Cocoa
 import SpriteKit
 import GameplayKit
 
-class ViewController: NSViewController {
+//class ViewController: NSViewController {
+//
+//    @IBOutlet var skView: SKView!
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        if let view = self.skView {
+//            // Load the SKScene from 'GameScene.sks'
+//            if let scene = SKScene(fileNamed: "GameScene") {
+//                // Set the scale mode to scale to fit the window
+//                scene.scaleMode = .aspectFill
+//
+//                // Present the scene
+//                view.presentScene(scene)
+//            }
+//
+//            view.ignoresSiblingOrder = true
+//            view.allowsMouseInteraction = true
+//
+//            view.showsFPS = true
+//            view.showsNodeCount = true
+//        }
+//    }
+//}
 
-    @IBOutlet var skView: SKView!
+class ViewController: NSViewController {
+//    override func loadView() {
+//        let sceneView = SKView(frame: CGRect(x: 0, y: 0, width: 1280, height: 960))
+//        let scene = GameScene(size: sceneView.frame.size)
+//        scene.scaleMode = .aspectFill
+//        sceneView.presentScene(scene)
+//
+//        self.view = sceneView
+//    }
+    
+    override func loadView() {
+        let mainView = NSView(frame: NSRect(x: 0, y: 0, width: 1260, height: 960))
+        
+        let sceneView = SKView(frame: CGRect(x: 0, y: 0, width: 960, height: 960))
+        let scene = GameScene(size: sceneView.frame.size)
+        scene.scaleMode = .aspectFill
+        sceneView.presentScene(scene)
+        mainView.addSubview(sceneView)
+        
+        let controlPanelView = SKView(frame: CGRect(x: 960, y: 0, width: 300, height: 960))
+        let controlPanelScene = ControlPanelScene(size: controlPanelView.frame.size)
+        controlPanelScene.scaleMode = .aspectFill
+        controlPanelView.presentScene(controlPanelScene)
+        mainView.addSubview(controlPanelView)
+        
+        self.view = mainView
+    }
+    
+    override var acceptsFirstResponder: Bool {
+        return true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if let view = self.skView {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
+        
+        if let skView = view as? SKView {
+            skView.window?.makeFirstResponder(skView)
+            skView.allowedTouchTypes = [.direct]
         }
     }
 }
-
